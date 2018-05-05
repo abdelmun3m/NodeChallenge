2bea9b489c4be8a5196cecdb3355bc8f03a9d48f
munem_mmm <abdelmunemelbasiouny@gmail.com>
2018-05-06 00:18:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Simple JS/Node Developer Challenge
+
+### Goal
+Clone this repo and build a simple dictionary key/value store script using only core NodeAPI and ECMAScript 5 or 6.  
+Store the key/value dictionary using filesystem.
+The client should be a standalone terminal tool.
+Once you have finished the challenge email the completed zip or link to a git repo of your choice.
+
+### Store Commands
+
+`$ node store.js add mykey myvalue`
+
+`$ node store.js list`
+
+`$ node store.js get mykey`
+
+`$ node store.js remove mykey`
+
+`$ node store.js clear`
+
+### Bonus
+
+- Commit and track history in github repo
+- Add ability to deploy in Docker container
+- Instead of running `node store.js` alter the runtime so it can be run as `./store`.
---
+++ store.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var path = require('path');
+
+
+
+var file_path = path.join(__dirname,"data.db");
+
+
+//console.log(process.argv);
+
+if(fs.existsSync(file_path)){
+  let dictionary = JSON.parse(fs.readFileSync(file_path));
+  switch (process.argv[2]) {
+    case "list": console.log(dictionary);
+      break;
+    case "add" :  dictionary[process.argv[3]] = process.argv[4] || "";
+      break;
+    case "get" : console.log(dictionary[process.argv[3]]);
+      break;
+    case "remove" :   delete(dictionary[process.argv[3]]);
+      break;
+    case "clear" : dictionary = {};
+        break;
+    default:
+        console.log("Unknown input");
+  }
+  fs.writeFileSync(file_path,JSON.stringify(dictionary));
+}else{
+  console.log("not found");
+}
+
+//
+// function addData(dictionary,key , value){
+//
+//   fs.writeFileSync(file_path,JSON.stringify(dictionary))
+// }
+//
+//
+// function removeKey(key){
+//   let dictionary = readData();
+//   delete(dictionary[key])
+//   fs.writeFileSync(file_path,JSON.stringify(dictionary))
+// }
+//
+//
+// function getKey(key){
+//   let dictionary = readData();
+//   return dictionary[key]
+// }
+//
+// function clearData(){
+//   fs.writeFileSync(file_path,JSON.stringify({}));
+// }
